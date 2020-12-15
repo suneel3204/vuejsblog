aade44e3bd892a6190084518115fc00a5d89ad3c
suneel3204 <suneel3204@gmail.com>
2020-12-15 14:04:48 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <!-- <h1>personal Blog</h1> -->
+    <img alt="Vue logo" src="./assets/letter.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+    <HelloWorld msg="Welcome to Suneel's Blog"/>
+    <!-- <ul>
+    <span> <img alt="Vue logo" src="./assets/light3.png"></span></ul> -->
+   <!-- <span>
+    <img alt="Vue logo" src="./assets/light3.png"></span> -->
+    <div>
+      <br>
+      <h3>Travelling</h3>
+      <p>When I first got into travel blogging, the way to make
+         money was by selling links and having 40-50 niche websites with Google adwords ads.
+          Press trips didn’t exist for bloggers and getting paid for our work was tough.
+
+Then came the dawn of the influencer. The industry blossomed in
+ opportunity and things were going pretty well until BOOM, the cooties hit.
+
+So now that the game has changed drastically again, how is it
+ possible to make money with a travel blog going into 2021?
+
+I’m here to tell
+ you it’s still totally possible to thrive in this role, 
+ provided you’re ready to put in the hard work and most importantly,
+  passion.</p><br>
+      <img alt="Vue logo" src="./assets/Forest.png">
+    </div>
+      <div>
+       <br>
+     <h3>Space</h3>
+       <p>
+         We all know what a tough year it’s been. At this year’s meeting 
+         of the American Geophysical Union (AGU), however, it’s been great to see so much hope delivered by science,
+          to see how much discovery and enthusiasm for exploring our world and our universe continue no matter what.
+       </p>
+      <img alt="Vue logo" src="./assets/space.png">
+    </div>
+     <div>
+       <br>
+     <h3>Cars</h3>
+       <p>
+      
+     If you’re a car enthusiast or are interested in learning more about
+      cars, you’ve come to the right place. Washos presents The 100 Best
+       Car Blogs 2016, which are listed in no particular order.
+      There are hundreds of car blogs out there that cover everything 
+      you’ve ever wanted to know about cars and more!</p>
+      <img alt="Vue logo" src="./assets/cars.png">
+    </div>
+     <div>
+       
+     <h3>Waterfalls</h3>
+       <p>
+       Vijayawada is a very beautiful city located in the south eastern Indian state of Andhra Pradesh. It has earned its fair share of recognition and reputation along the way for its hospitality and of course owing to its amazing attractions
+        that it offers. It houses the very sacred and ornate Kanaka Durga Temple which is set atop a hill graciously overlooking the city
+      <img alt="Vue logo" src="./assets/waterfalls.png"></p>
+    </div>
+     <div>
+       
+     <h3>Oceans</h3>
+       <p>
+      Choose one of six Cardigan Bay dolphins and they will receive a monthly newsletter of when and where their dolphin has been sighted and with who, as well as a goodie pack including an adorable cuddly dolphin, a dolphin species identification guide and poster, and of course access to an exclusive adopters area!
+Or do you fancy doing some ocean related Christmas baking? Order the dolphin cookie cutter from the Sea Watch store and send us your pictures on Facebook! 
+       </p>
+      <img alt="Vue logo" src="./assets/Ocean.png">
+    </div>
+     <h3>Details</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Admin</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Contact</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">Address</a></li>
+      <!-- <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loa</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li> -->
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/* <style scoped> */
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
